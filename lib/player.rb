3b142f4c88f354a4776b148ac90280fdca4ba708
123Nell123 <nellp12321@gmail.com>
2022-02-01 14:54:24 -0800

    onapp2

--- lib/player.rb
+++ lib/player.rb
@@ -2,7 +2,13 @@ class Player
     attr_accessor :name, :life_points
     @@alive = true
 
- 
+    def initialize(name)
+        @name = name
+        @life_points = 10
+    end
+
+
+
     def show_state
     puts "il  reste  #{@life_points} à #{@name} points de vie"  
     end
@@ -36,6 +42,46 @@ end
 
 
 class HumanPlayer  < Player
+    attr_accessor : @weapon_level
+    
+    
+
+
+
+    def initialize(name)
+        super (name)
+        @life_points = 100
+        @weapon_level = 1
+
+    end
+
+
+
+
+    def show_state
+        puts "il  reste  #{@life_points} à #{@name} points de vie et une arme de niveau #{@weapon_level}"  
+    end
+
+    def compute_damage
+        rand(1..6) * @weapon_level
+    end
+
+    def search_weapon
+        choix = rand(1..6)
+        case x
+        when 1
+          puts "Tu n'as rien trouvé... "
+        when 2..5
+        @life_points += 50
+          puts "Bravo, tu as trouvé un pack de +50 points de vie !"
+        when 6
+          @life_points += 80
+          puts "Bravo, tu as trouvé un pack de +80 points de vie !"
+
+        end
+    end
+
+
 
 
 end
